3f6551e5ae2a3ad2b1222ae127f46edbb261d3ea
nkuka <nkuka@Njabulo>
2023-10-20 20:40:24 +0200

    First Push

---
+++ src/randomnumbergenerator/RandomNumberGenerator.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package randomnumbergenerator;
+
+/**
+ *
+ * @author nkuka
+ */
+public class RandomNumberGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmMain frmMain = new frmMain();
+        frmMain.show();
+    }
+    
+}
